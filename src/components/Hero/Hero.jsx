9ac15565563f04a4890a6aeebaef998905f0b992
David Almonacid <david.almonacidf@gmail.com>
2023-05-15 17:38:45 -0500

    Carrusel de imagenes mejorado

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,48 +1,70 @@
-import heroImage1 from '@assets/images/hero-1.jpg';
-import heroImage2 from '@assets/images/hero-2.jpg';
-import heroImage3 from '@assets/images/hero-3.jpg';
-import heroImage4 from '@assets/images/hero-4.jpg';
-import heroImage5 from '@assets/images/hero-5.jpg';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './Hero.css';
 
+const slides = [
+  {
+    src: 'src/assets/images/hero-1.jpg',
+    alt: 'Image 1'
+  },
+  {
+    src: 'src/assets/images/hero-2.jpg',
+    alt: 'Image 2'
+  },
+  {
+    src: 'src/assets/images/hero-3.jpg',
+    alt: 'Image 3'
+  },
+  {
+    src: 'src/assets/images/hero-4.jpg',
+    alt: 'Image 4'
+  },
+  {
+    src: 'src/assets/images/hero-5.jpg',
+    alt: 'Image 5'
+  }
+];
+
 const Hero = () => {
+  const [index, setIndex] = useState(0);
+  const [currentSlide, setCurrentSlide] = useState({});
+
+  const handlePrevious = () => {
+    if (index === 0) {
+      setIndex(() => slides.length - 1);
+      return;
+    }
+
+    setIndex(() => index - 1);
+  };
+
+  const handleNext = () => {
+    if (index === slides.length - 1) {
+      setIndex(0);
+      return;
+    }
+
+    setIndex(() => index + 1);
+  };
+
   useEffect(() => {
-    const slides = document.querySelectorAll('.hero__carousel-item');
-    console.log(slides);
-
-    let currentSlide = 0;
-    const interval = setInterval(() => {
-      slides[currentSlide].classList.remove('hero__carousel-item--active');
-      currentSlide = (currentSlide + 1) % slides.length;
-      slides[currentSlide].classList.add('hero__carousel-item--active');
-    }, 4000);
-
-    return () => clearInterval(interval);
-  });
+    setCurrentSlide(slides[index]);
+  }, [index]);
 
   return (
     <div className='hero'>
       <div className='hero__carousel'>
-        <div className='hero__carousel-item hero__carousel-item--active'>
-          <img src={heroImage1} alt='' className='hero__carousel-image' />
-        </div>
-
         <div className='hero__carousel-item'>
-          <img src={heroImage2} alt='' className='hero__carousel-image' />
+          <img
+            src={currentSlide.src}
+            alt={currentSlide.alt}
+            className='hero__carousel-image'
+          />
         </div>
+      </div>
 
-        <div className='hero__carousel-item'>
-          <img src={heroImage3} alt='' className='hero__carousel-image' />
-        </div>
-
-        <div className='hero__carousel-item'>
-          <img src={heroImage4} alt='' className='hero__carousel-image' />
-        </div>
-
-        <div className='hero__carousel-item'>
-          <img src={heroImage5} alt='' className='hero__carousel-image' />
-        </div>
+      <div className='hero__carousel-controls'>
+        <button onClick={handlePrevious}>Previous</button>
+        <button onClick={handleNext}>Next</button>
       </div>
     </div>
   );
