6deb4d42875fb94e0fbaa1534bd48eb66db0f6ff
Francisco <franciscogarzonmoreno@gmail.com>
2023-05-15 20:57:58 -0500

    se creo el componente footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+import facebookLogo from "@assets/icons/facebook.svg";
+import instagramLogo from "@assets/icons/instagram.svg";
+import twitterLogo from "@assets/icons/twitter.svg";
+import youtubeLogo from "@assets/icons/youtube.svg";
+import './Footer.css';
+
+const Footer = () => {
+	return <div className='footer'>
+		<div>HOTEL PARADISE</div>
+		<div>Carrera 104A # 16 - 23</div>
+		<div>Bogotá</div>
+		<div>Tel: +57 (601) 2280970 </div>
+		<a href='https://www.facebook.com' rel="noreferrer" target="_blank"><img src={facebookLogo} alt="" /></a>
+		<a href='https://www.twitter.com' rel="noreferrer" target="_blank"><img src={twitterLogo} alt="" /></a>
+		<a href='https://www.youtube.com' rel="noreferrer" target="_blank"><img src={youtubeLogo} alt="" /></a>
+		<a href='https://www.instagram.com' rel="noreferrer" target="_blank"><img src={instagramLogo} alt="" /></a>
+	</div>
+};
+
+export default Footer;
