2111755d54bcb931adeb8254e6b5a919cd0af4d1
Francisco <franciscogarzonmoreno@gmail.com>
2023-05-15 21:01:12 -0500

    se crearon links y configuraciones de la rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import { Layout } from '@components';
+import { Habitaciones, Home, Planes, Servicios } from '@pages';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <Layout />
-    </>
+    <Routes>
+      <Route path='/'element={ <Layout />}>
+    <Route index element={<Home />}/>
+    <Route path='/planes' element={<Planes />}/>
+    <Route path='/servicios' element={<Servicios />}/>
+    <Route path='/habitaciones' element={<Habitaciones />}/>
+      </Route>
+     
+    </Routes>
   );
 }
 
