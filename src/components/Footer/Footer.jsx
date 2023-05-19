4e2f6a37e4331e04290b6a7c3cf37080450efb66
David Almonacid <david.almonacidf@gmail.com>
2023-05-19 09:33:50 -0500

    Improving accessibility

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -15,18 +15,35 @@ const Footer = () => {
         <p>Bogotá</p>
         <p>Tel: +57 (601) 2280970 </p>
       </div>
+
       <div className='footer__social-media'>
         <a href='https://www.facebook.com' rel='noreferrer' target='_blank'>
-          <img src={facebookLogo} alt='' />
+          <img
+            src={facebookLogo}
+            alt='facebook logo'
+            aria-label='Know more in our facebook page'
+          />
         </a>
         <a href='https://www.twitter.com' rel='noreferrer' target='_blank'>
-          <img src={twitterLogo} alt='' />
+          <img
+            src={twitterLogo}
+            alt='twitter logo'
+            aria-label='Know more in our twitter page'
+          />
         </a>
         <a href='https://www.youtube.com' rel='noreferrer' target='_blank'>
-          <img src={youtubeLogo} alt='' />
+          <img
+            src={youtubeLogo}
+            alt='youtube logo'
+            aria-label='Know more in our youtube channel'
+          />
         </a>
         <a href='https://www.instagram.com' rel='noreferrer' target='_blank'>
-          <img src={instagramLogo} alt='' />
+          <img
+            src={instagramLogo}
+            alt='instagram logo'
+            aria-label='Know more in our instagram page'
+          />
         </a>
       </div>
     </div>
