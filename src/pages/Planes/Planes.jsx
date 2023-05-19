af8f0cfb902bffbda857f8f29dd8c5c562037d6f
Francisco <franciscogarzonmoreno@gmail.com>
2023-05-19 00:36:33 -0500

    creación de texo y imagenes de planes

--- src/pages/Planes/Planes.jsx
+++ src/pages/Planes/Planes.jsx
@@ -2,25 +2,48 @@ const Planes = () => {
   return (
     <div>
       <div>
-        <h3>Plan empresarial</h3>
+        <h3>PLAN AMIGAS</h3>
         <img src="" alt="" />
-        <p></p>
+        <p>
+          VALOR $599.000 Alojamiento en habitacion Junior para 3 personas Tabla
+          de Quesos y Frutas de Bienvenida a la Habitacion Masaje de relajacion
+          de 40min
+        </p>
       </div>
       <div>
-        <h3>Planindividual</h3>
+        <h3>PLAN ESCAPADITA</h3>
         <img src="" alt="" />
+        <p>
+          VALOR $290000 Noche de Alojamiento en Habitación Junior para dos
+          personas Desayuno tipo Buffet en el restaurante 2 Cervezas y pizza
+        </p>
       </div>
       <div>
-        <h3>Plan duo</h3>
+        <h3>PLAN ROMANTICO</h3>
         <img src="" alt="" />
+        <p>
+          Valor $450.000 Noche de alojamiento en habitación Junior para dos
+          personas Desayuno tipo Buffet en el restaurante Cena (Menú sugerencia
+          del chef) 2 bebidas sin alcohol para acompañar la cena
+        </p>
       </div>
       <div>
-        <h3>Plan familiar</h3>
+        <h3>PLAN CUMPLEAÑOS</h3>
         <img src="" alt="" />
+        <p>
+          Valor $320.000 Noche de alojamiento en habitación Junior para dos
+          personas Desayuno tipo Buffet en el restaurante Decoración especial
+          (Globos) Torta de cumpleaños Media botella de vino Piscina
+        </p>
       </div>
       <div>
-        <h3>Plan excursion</h3>
+        <h3>PLAN RELAX</h3>
         <img src="" alt="" />
+        <p>
+          Valor $550.000 Alojamiento en Habitación Junior para dos personas
+          Masaje de relajación simultaneo de 40min con exfoliación y
+          reflexología (la cita será agendada por el SPA )
+        </p>
       </div>
     </div>
   );
